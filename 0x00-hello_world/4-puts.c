1a8b147a1208c2ff058ae0b6a299db13623c38ed
NasaraSualiha <bintbelawu@gmail.com>
2023-10-10 16:28:18 -0700

    my first c script, using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Description: printing using puts
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual language");
+	return (0);
+}
