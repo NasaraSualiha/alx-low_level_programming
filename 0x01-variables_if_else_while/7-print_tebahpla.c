00c4dbd4f2a44dd5d7e850ee019a34970d0e694e
NasaraSualiha <bintbelawu@gmail.com>
2023-10-16 15:42:10 -0700

    using for to print reverse form of lowercase alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  *main - entry point
+  *Description: print lowercase alphabet
+  *in reverse
+  *Return: always 0
+  */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
