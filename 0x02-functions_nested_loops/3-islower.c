268decd83c9508ca5d5b230e8d892fa25a5b7b86
NasaraSualiha <bintbelawu@gmail.com>
2023-10-18 14:36:10 -0700

    returning prototype and arguments in prototypes

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *_islower - check for lowercase
+  *@c: character to be checked
+  *Return: 1 for lowercase or 0 for any other
+  */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
