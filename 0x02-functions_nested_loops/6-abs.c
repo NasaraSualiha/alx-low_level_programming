6e55e04fe720270d911d3feec7baa8821f3c03e2
NasaraSualiha <bintbelawu@gmail.com>
2023-10-19 12:54:34 -0700

    return value can be a letter provided in a loop, this one had -n to print absolute numbers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  *_abs - computes absolute value
+  *@n: value to be computed
+  *
+  *Return: Always positive
+  */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
