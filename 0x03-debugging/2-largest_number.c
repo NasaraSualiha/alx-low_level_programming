7b8c493a8bd3f156015aab430e690e3f3583558f
NasaraSualiha <bintbelawu@gmail.com>
2023-10-24 01:35:36 -0700

    print all natural numbers from n to 98

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,13 +10,13 @@
 
 int largest_number(int a, int b, int c)
 {
-	 int largest;
+	int largest;
 
-	 if (a > b && a > c)
+	if (a > b && a > c)
 	 {
 		 largest = a;
 	 }
-	 else if (a > b && c > a)
+	else if (a > b && c > a)
 	 {
 		 largest = c;
 	 }
